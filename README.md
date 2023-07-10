0a7d288cb64996098517812c44f600d535897ca8
zclaytor <zclaytor@hawaii.edu>
2023-07-10 15:49:01 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gaiabusters
+
+Query the Gaia archive for data/light curves, make plots?
+
+query using ra/dec?
---
+++ gaiabusters.py
@@ -0,0 +1,14 @@
+class DataTable():
+    def __init__(self, ra, dec):
+        self.ra = ra
+        self.dec = dec
+        # query gaia-source, get important columns
+
+    def get_lightcurve(self):
+        pass
+
+    def get_spectrum(self):
+        pass
+
+    def get_sdss(self):
+        pass
