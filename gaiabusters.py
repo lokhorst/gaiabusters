3245f2e741551421f8b5fa838d5fe5172e6e9b1e
zclaytor <zclaytor@hawaii.edu>
2023-07-11 15:14:01 -0400

    add basic query functionality

--- gaiabusters.py
+++ gaiabusters.py
@@ -1,8 +1,20 @@
+from astroquery.gaia import Gaia
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+
 class DataTable():
-    def __init__(self, ra, dec):
-        self.ra = ra
-        self.dec = dec
+    def __init__(
+            self, coord, radius="2 arcsec", source_table="gaiadr3.gaia_source"):
+        self.coord = coord
         # query gaia-source, get important columns
+        self.source_table = source_table
+        Gaia.MAIN_GAIA_TABLE = self.source_table
+        self.query_result = Gaia.query_object_async(coord, radius)
+
+    def set_source_table(source_table):
+        self.source_table = source_table
+        Gaia.MAIN_GAIA_TABLE = self.source_table
 
     def get_lightcurve(self):
         pass
