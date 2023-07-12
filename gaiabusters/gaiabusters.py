e8247ad28e5d03c89fb6b9edf48a62338a66549a
Deborah Lokhorst <deb.lokhorst@gmail.com>
2023-07-12 09:36:35 -0700

    Adding sphinx documentation

--- gaiabusters/gaiabusters.py
+++ gaiabusters/gaiabusters.py
@@ -6,6 +6,16 @@ import astropy.units as u
 
 class DataTable():
     def __init__(self, coord, radius="10 arcsec", source_table="gaiadr3.gaia_source"):
+        """ Initialize DataTable object to hold GAIA data information (+ other sources).
+        Currently finds closest source to specified coordinates.
+
+        Args:
+            coord (SkyCoord): Coordinates of the target/field
+            radius (str, optional): Search radius including units. Defaults to "10 arcsec".
+            source_table (str, optional): Which gaia source table to query. Can specify dr2, dr3, etc.
+                Needs to be a source table. Defaults to "gaiadr3.gaia_source".
+        """
+        
         self.coord = coord
         # configure Gaia query environment
         self.set_source_table(source_table)
@@ -17,22 +27,35 @@ class DataTable():
         self.source_id = self.query_result[0]["source_id"]
 
     def set_source_table(self, source_table):
+        """Setting the source table variables with search output.
+        (to do: have a list of available source tables and assert that
+        specified table is in the list before continuing - display available
+        source tables)
+
+        Args:
+            source_table (str): Name of the source table to be searched.
+        """
         self.source_table = source_table
         Gaia.MAIN_GAIA_TABLE = self.source_table
 
     def get_epoch_photometry(self, verbose = False):
+        """ Retrieves epoch photometry data from datalink for current object.
+
+        Args:
+            verbose (bool, optional): Prints verbose statements. Defaults to False.
+        """
 
         #This is hardcoded so that this function does this one thing, should make it easier to test and validate
         retrieval_type = 'EPOCH_PHOTOMETRY'   # Options are: 'EPOCH_PHOTOMETRY', 'MCMC_GSPPHOT', 'MCMC_MSC', 'XP_SAMPLED', 'XP_CONTINUOUS', 'RVS', 'ALL'
         data_structure = 'INDIVIDUAL'   # Options are: 'INDIVIDUAL', 'COMBINED', 'RAW'
         data_release   = 'Gaia DR3'     # Options are: 'Gaia DR3' (default), 'Gaia DR2'
 
-        datalink = Gaia.load_data(ids=self.source_table.source_id, data_release = data_release, 
+        datalink = Gaia.load_data(ids=self.source_id, data_release = data_release, 
                                 retrieval_type=retrieval_type, 
                                 data_structure = data_structure, 
                                 verbose = False, output_file = None)
 
-        dl_key = f"{retrieval_type}-{data_release} {source_id}.xml"
+        dl_key = f"{retrieval_type}-{data_release} {self.source_id}.xml"
 
         if verbose:
             print(f'The following Datalink products have been downloaded:')
